7254f6d861b4a9c3d7cd5a6af3478f1d6c3ef30a
Wisdom <houseofwem1@gamil.com>
2023-07-13 13:01:47 -0700

    commiting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 }
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
