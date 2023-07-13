bdf9905adb0657ec590405e4d0c1577c434952c8
Wisdom <houseofwem1@gamil.com>
2023-07-13 08:48:20 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main -  writing C code with standard I/O
+ *return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl");
+	return (0);
+}
