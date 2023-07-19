f3c010d2be5d7a425278ce2097d397c3efb71b6a
Wem-Tech <houseofwem1@gmail.com>
2023-07-19 19:05:59 +0100

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if (a > b && a > c)
 	{
 	largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > c)
 	{
 	largest = b;
 	}
