b8dea0b779345ae06fc162160ff44c3ea97a29c3
Wem-Tech <houseofwem1@gmail.com>
2023-07-20 23:02:36 +0100

    10 lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int g, h;
+
+	for (g = 0; g <= 10; g++)
+
+	{
+		for (h = 0; h <= 14; h++)
+		{
+		if (h > 9)
+		{
+		_putchar((h / 10) + '0');
+		}
+		_putchar((h % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
