90f8338e6d970d61399de967aabc0b3df7408cd3
Wem-Tech <houseofwem1@gmail.com>
2023-07-21 00:53:54 +0100

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int y, z;
+
+	for (y = 0; y < size; y++)
+	{
+
+		for (z = 0; z < size; z++)
+	{
+		_putchar('#');
+
+	}
+		_putchar('\n');
+	}
+
+	}
+}
