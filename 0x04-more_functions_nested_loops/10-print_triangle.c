b59a91da8366c1c4c6433bd2a6ad2793ddef3cea
Wem-Tech <houseofwem1@gmail.com>
2023-07-21 03:38:43 +0100

    hmm

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (a = 0; a <= size; a++)
+	{
+
+		for (b = 0; b < size - a - 1; b++)
+
+		_putchar(' ');
+
+		for (b = 0; b <= a; b++)
+
+			_putchar('#');
+
+		_putchar('\n');
+	}
+
+}
