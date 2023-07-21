0663d6ad85ecb74f8c9b38187c9cc166ad9ad775
Wem-Tech <houseofwem1@gmail.com>
2023-07-21 14:17:59 +0100

    trinagle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,14 +10,14 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	if (size <= 0)
+	if (size < 0)
 	{
 		_putchar('\n');
 	}
 	for (a = 0; a <= size; a++)
 	{
 
-		for (b = 0; b < size - a - 1; b++)
+		for (b = 0; b < size - a; b++)
 
 		_putchar(' ');
 
