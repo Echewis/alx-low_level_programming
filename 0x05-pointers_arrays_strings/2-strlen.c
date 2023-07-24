b7dce0902ace227509dcbe650ed60ff5eb8295d7
Wem-Tech <houseofwem1@gmail.com>
2023-07-24 19:24:49 +0100

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to get length of
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int wen = 0;
+	while (*s != '\0')
+	{
+		wen++;
+		s++;
+	}
+	return (wen);
+}
