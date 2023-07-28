8ac90f57e9d5ed2f18fe461f5b3714e69953c4cc
Wem-Tech <houseofwem1@gmail.com>
2023-07-28 20:42:33 +0100

    alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,8 +17,9 @@ int main(void)
 	printf("last digit of %d is %d", n, abs(n % 10));
 	if (n % 10 > 5)
 		printf(" and is greater than 5\n");
+
 	else if (n % 10 == 0)
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	else
 		printf(" and is less than 6 and not 0\n");
 
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+
+	}
+	return (0);
+}
