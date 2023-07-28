be4f2467cfdc7fe1bd3901c053394b8311f4a6ad
Wem-Tech <houseofwem1@gmail.com>
2023-07-28 21:36:58 +0100

    low upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a;
+	int A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+	 for (A = 'A'; A <= 'Z'; A++)
+	{
+	putchar(A);
+	}
+	return (0);
+}	
