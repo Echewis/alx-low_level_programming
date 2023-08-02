f6a0b48769bb65e8f951cdf1bba6c66ff09df100
Wem-Tech <houseofwem1@gmail.com>
2023-08-02 21:07:32 +0100

    prime No

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer.
+ *
+ * Return: natural square root of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (_sqrt_helper(n, 1));
+	}
+}
+
+/**
+ * _sqrt_helper - helper function for _sqrt_recursion.
+ * @n: integer.
+ * @a: integer.
+ *
+ * Return: natural square root of n.
+ */
+int _sqrt_helper(int n, int a)
+{
+	if (a * a == n)
+	{
+		return (a);
+	}
+	else if (a * a > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_helper(n, a + 1));
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: integer.
+ *
+ * Return: 1 if n is prime number, otherwise return 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, 2));
+}
+
+
+/**
+ * is_prime_helper - helper function for is_prime_number.
+ * @n: integer.
+ * @a: integer.
+ *
+ * Return: 1 if n is prime number, otherwise return 0.
+ */
+
+int is_prime_helper(int n, int a)
+{
+	if (a == n)
+	{
+		return (1);
+	}
+	else if (n % a == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_helper(n, a + 1));
+	}
+}
