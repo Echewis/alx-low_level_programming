fb430927879317f2c572e5630e4cebfe776b51a7
Wem-Tech <houseofwem1@gmail.com>
2023-08-06 22:11:54 +0100

    printing name of argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - countains main function
+ * @argc: for argument count
+ * @argv: for argument vector
+ * Return: always return 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+### This project is introductory to Argument count(argc) and Argument vector (argv)
