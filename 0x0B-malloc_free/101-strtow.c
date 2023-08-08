b6746af88db6c0f26715db5c1d17c89742a4f875
Wem-Tech <houseofwem1@gmail.com>
2023-08-08 23:36:31 +0100

    split strings to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+/**
+ * strtow - function that splits
+ * @str: The link to strings
+ * Return: in this case will return words
+ */
 
 char **strtow(char *str)
 {
@@ -9,8 +13,10 @@ char **strtow(char *str)
 	char *split = strtok(str, " ");
 
 	int count = 0;
+
 	if (str == NULL || *str == '\0')
 		return (NULL);
+
 	while (split != NULL)
 	{
 		words = realloc(words, sizeof(char *) * (count + 1));
