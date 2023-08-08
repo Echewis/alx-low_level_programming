c22f128d350e49e48a3293173b49d1e7e7cdafb9
Wem-Tech <houseofwem1@gmail.com>
2023-08-08 18:02:34 +0100

    update

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - is the function
+ * @grid: is a double pointer
+ * @height: integer to be freed
+ */
+
+void free_grid(int **grid, int height)
+{
+	int s;
+
+	for (s = 0; s < height; s++)
+	{
+		free(grid[s]);
+	}
+	free(grid);
+}
