ea32af7e07321f15ebb0e77da1e55f36e6fcb9fb
Wem-Tech <houseofwem1@gmail.com>
2023-08-14 22:29:49 +0100

    a function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - is a funtion that frees the dog
+ * @d: is a pointer
+ * Return: returns nothing
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
