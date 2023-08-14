051a3b5412bbf6e6f0b03c5472a1303e16370236
Wem-Tech <houseofwem1@gmail.com>
2023-08-14 21:37:53 +0100

    Define a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /*
