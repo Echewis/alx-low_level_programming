003fdbae6627dd3c9fe3148205eb833f61f873d0
Wem-Tech <houseofwem1@gmail.com>
2023-08-16 17:08:25 +0100

    function that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * int_index - is a function that returns first element
+ * @array: is the list of data
+ * @size: is the size of element
+ * @cmp: is a pointer
+ * Return: will return v or -1
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -12,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (v = 0; v <= size; v++)
 	{
 		if (cmp(array[v]))
-			return(v);
+			return (v);
 	}
 	return (-1);
 }
