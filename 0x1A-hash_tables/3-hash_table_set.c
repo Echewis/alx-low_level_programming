f68e40de8a0c9cd63ecc0acb277b32606c5187be
WisDomz <houseofwem1@gmail.com>
2023-11-03 10:20:32 +0100

    will add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,35 +9,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
+	unsigned long int index, a;
 	hash_node_t *new_node;
-	hash_node_t *node = ht->array[index];
+	char *copy;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
-/*	hash_node_t *node = ht->array[index];*/
+	copy = strdup(value);
+	if (copy == NULL)
+		return (0);
 
-	while (node != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (a = index; ht->array[a]; a++)
 	{
-		if (strcmp(node->key, key) == 0)
+		if (strcmp(ht->array[a]->key, key) == 0)
 		{
-			free(node->value);
-			node->value = strdup(value);
+			free(ht->array[a]->value);
+			ht->array[a]->value = copy;
 			return (1);
 		}
-		node = node->next;
 	}
 
-	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(copy);
 		return (0);
-
+	}
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = copy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
