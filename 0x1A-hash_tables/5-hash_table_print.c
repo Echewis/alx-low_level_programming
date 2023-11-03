16673ec44f61a6f46b23f43e5218b78b050bc541
WisDomz <houseofwem1@gmail.com>
2023-11-03 10:22:19 +0100

    print table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - this will print the element
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_table_t *n;
+	unsigned long int a;
+	unsigned char flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (a = 0; a < ht->size; a++)
+	{
+		if (ht->array[a] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			n = ht->array[a];
+			while (n != NULL)
+			{
+				printf("'%s': '%s'", n->key, n->value);
+				n = n->next;
+				if (n != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
